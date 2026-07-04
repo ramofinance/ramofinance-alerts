5e31b7158b25418639196bd93b94063ef607f5dc
ramofinance <rahbarmohsen87@gmail.com>
2026-07-04 06:40:57 +0000

    chore: add backend security middleware

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,4 +1,7 @@
+import compression from "compression";
+import cors from "cors";
 import express from "express";
+import helmet from "helmet";
 import { errorHandler } from "./middleware/error-handler";
 import { notFoundHandler } from "./middleware/not-found";
 import { healthRoutes } from "./routes/health.routes";
@@ -6,6 +9,9 @@ import { healthRoutes } from "./routes/health.routes";
 export const createServer = () => {
   const app = express();
 
+  app.use(helmet());
+  app.use(cors());
+  app.use(compression());
   app.use(express.json());
 
   app.use(healthRoutes);
