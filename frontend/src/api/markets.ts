9f43bd0785ad6f7d2f84e731dcaf1cf9f73641e7
Mohsen <mohsen@example.com>
2026-07-20 14:37:08 +0330

    load all markets in frontend

--- frontend/src/api/markets.ts
+++ frontend/src/api/markets.ts
@@ -2,5 +2,5 @@ import type { Market, PaginatedResponse } from "../types/api";
 import { apiGet } from "./http-client";
 
 export const getMarkets = () => {
-  return apiGet<PaginatedResponse<Market>>("/api/markets");
+  return apiGet<PaginatedResponse<Market>>("/api/markets?limit=100");
 };
