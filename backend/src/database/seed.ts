ddb4cfef9117d5ee4ab4e534859a2111e3d98f7e
Mohsen <mohsen@example.com>
2026-07-20 14:01:12 +0330

    add more crypto markets

--- backend/src/database/seed.ts
+++ backend/src/database/seed.ts
@@ -38,6 +38,356 @@ const markets = [
     quoteAsset: "USDT"
   },
   {
+    symbol: "ADAUSDT",
+    name: "ADA / Tether",
+    type: MarketType.CRYPTO,
+    baseAsset: "ADA",
+    quoteAsset: "USDT"
+  },
+  {
+    symbol: "DOGEUSDT",
+    name: "DOGE / Tether",
+    type: MarketType.CRYPTO,
+    baseAsset: "DOGE",
+    quoteAsset: "USDT"
+  },
+  {
+    symbol: "TRXUSDT",
+    name: "TRX / Tether",
+    type: MarketType.CRYPTO,
+    baseAsset: "TRX",
+    quoteAsset: "USDT"
+  },
+  {
+    symbol: "XLMUSDT",
+    name: "XLM / Tether",
+    type: MarketType.CRYPTO,
+    baseAsset: "XLM",
+    quoteAsset: "USDT"
+  },
+  {
+    symbol: "XMRUSDT",
+    name: "XMR / Tether",
+    type: MarketType.CRYPTO,
+    baseAsset: "XMR",
+    quoteAsset: "USDT"
+  },
+  {
+    symbol: "LINKUSDT",
+    name: "LINK / Tether",
+    type: MarketType.CRYPTO,
+    baseAsset: "LINK",
+    quoteAsset: "USDT"
+  },
+  {
+    symbol: "BCHUSDT",
+    name: "BCH / Tether",
+    type: MarketType.CRYPTO,
+    baseAsset: "BCH",
+    quoteAsset: "USDT"
+  },
+  {
+    symbol: "LTCUSDT",
+    name: "LTC / Tether",
+    type: MarketType.CRYPTO,
+    baseAsset: "LTC",
+    quoteAsset: "USDT"
+  },
+  {
+    symbol: "SUIUSDT",
+    name: "SUI / Tether",
+    type: MarketType.CRYPTO,
+    baseAsset: "SUI",
+    quoteAsset: "USDT"
+  },
+  {
+    symbol: "HBARUSDT",
+    name: "HBAR / Tether",
+    type: MarketType.CRYPTO,
+    baseAsset: "HBAR",
+    quoteAsset: "USDT"
+  },
+  {
+    symbol: "AVAXUSDT",
+    name: "AVAX / Tether",
+    type: MarketType.CRYPTO,
+    baseAsset: "AVAX",
+    quoteAsset: "USDT"
+  },
+  {
+    symbol: "SHIBUSDT",
+    name: "SHIB / Tether",
+    type: MarketType.CRYPTO,
+    baseAsset: "SHIB",
+    quoteAsset: "USDT"
+  },
+  {
+    symbol: "NEARUSDT",
+    name: "NEAR / Tether",
+    type: MarketType.CRYPTO,
+    baseAsset: "NEAR",
+    quoteAsset: "USDT"
+  },
+  {
+    symbol: "UNIUSDT",
+    name: "UNI / Tether",
+    type: MarketType.CRYPTO,
+    baseAsset: "UNI",
+    quoteAsset: "USDT"
+  },
+  {
+    symbol: "TAOUSDT",
+    name: "TAO / Tether",
+    type: MarketType.CRYPTO,
+    baseAsset: "TAO",
+    quoteAsset: "USDT"
+  },
+  {
+    symbol: "ONDOUSDT",
+    name: "ONDO / Tether",
+    type: MarketType.CRYPTO,
+    baseAsset: "ONDO",
+    quoteAsset: "USDT"
+  },
+  {
+    symbol: "ETCUSDT",
+    name: "ETC / Tether",
+    type: MarketType.CRYPTO,
+    baseAsset: "ETC",
+    quoteAsset: "USDT"
+  },
+  {
+    symbol: "APTUSDT",
+    name: "APT / Tether",
+    type: MarketType.CRYPTO,
+    baseAsset: "APT",
+    quoteAsset: "USDT"
+  },
+  {
+    symbol: "ICPUSDT",
+    name: "ICP / Tether",
+    type: MarketType.CRYPTO,
+    baseAsset: "ICP",
+    quoteAsset: "USDT"
+  },
+  {
+    symbol: "FILUSDT",
+    name: "FIL / Tether",
+    type: MarketType.CRYPTO,
+    baseAsset: "FIL",
+    quoteAsset: "USDT"
+  },
+  {
+    symbol: "ATOMUSDT",
+    name: "ATOM / Tether",
+    type: MarketType.CRYPTO,
+    baseAsset: "ATOM",
+    quoteAsset: "USDT"
+  },
+  {
+    symbol: "ARBUSDT",
+    name: "ARB / Tether",
+    type: MarketType.CRYPTO,
+    baseAsset: "ARB",
+    quoteAsset: "USDT"
+  },
+  {
+    symbol: "OPUSDT",
+    name: "OP / Tether",
+    type: MarketType.CRYPTO,
+    baseAsset: "OP",
+    quoteAsset: "USDT"
+  },
+  {
+    symbol: "INJUSDT",
+    name: "INJ / Tether",
+    type: MarketType.CRYPTO,
+    baseAsset: "INJ",
+    quoteAsset: "USDT"
+  },
+  {
+    symbol: "IMXUSDT",
+    name: "IMX / Tether",
+    type: MarketType.CRYPTO,
+    baseAsset: "IMX",
+    quoteAsset: "USDT"
+  },
+  {
+    symbol: "RENDERUSDT",
+    name: "RENDER / Tether",
+    type: MarketType.CRYPTO,
+    baseAsset: "RENDER",
+    quoteAsset: "USDT"
+  },
+  {
+    symbol: "STXUSDT",
+    name: "STX / Tether",
+    type: MarketType.CRYPTO,
+    baseAsset: "STX",
+    quoteAsset: "USDT"
+  },
+  {
+    symbol: "MKRUSDT",
+    name: "MKR / Tether",
+    type: MarketType.CRYPTO,
+    baseAsset: "MKR",
+    quoteAsset: "USDT"
+  },
+  {
+    symbol: "AAVEUSDT",
+    name: "AAVE / Tether",
+    type: MarketType.CRYPTO,
+    baseAsset: "AAVE",
+    quoteAsset: "USDT"
+  },
+  {
+    symbol: "PEPEUSDT",
+    name: "PEPE / Tether",
+    type: MarketType.CRYPTO,
+    baseAsset: "PEPE",
+    quoteAsset: "USDT"
+  },
+  {
+    symbol: "ALGOUSDT",
+    name: "ALGO / Tether",
+    type: MarketType.CRYPTO,
+    baseAsset: "ALGO",
+    quoteAsset: "USDT"
+  },
+  {
+    symbol: "VETUSDT",
+    name: "VET / Tether",
+    type: MarketType.CRYPTO,
+    baseAsset: "VET",
+    quoteAsset: "USDT"
+  },
+  {
+    symbol: "FTMUSDT",
+    name: "FTM / Tether",
+    type: MarketType.CRYPTO,
+    baseAsset: "FTM",
+    quoteAsset: "USDT"
+  },
+  {
+    symbol: "EGLDUSDT",
+    name: "EGLD / Tether",
+    type: MarketType.CRYPTO,
+    baseAsset: "EGLD",
+    quoteAsset: "USDT"
+  },
+  {
+    symbol: "THETAUSDT",
+    name: "THETA / Tether",
+    type: MarketType.CRYPTO,
+    baseAsset: "THETA",
+    quoteAsset: "USDT"
+  },
+  {
+    symbol: "GRTUSDT",
+    name: "GRT / Tether",
+    type: MarketType.CRYPTO,
+    baseAsset: "GRT",
+    quoteAsset: "USDT"
+  },
+  {
+    symbol: "RUNEUSDT",
+    name: "RUNE / Tether",
+    type: MarketType.CRYPTO,
+    baseAsset: "RUNE",
+    quoteAsset: "USDT"
+  },
+  {
+    symbol: "FLOWUSDT",
+    name: "FLOW / Tether",
+    type: MarketType.CRYPTO,
+    baseAsset: "FLOW",
+    quoteAsset: "USDT"
+  },
+  {
+    symbol: "QNTUSDT",
+    name: "QNT / Tether",
+    type: MarketType.CRYPTO,
+    baseAsset: "QNT",
+    quoteAsset: "USDT"
+  },
+  {
+    symbol: "SEIUSDT",
+    name: "SEI / Tether",
+    type: MarketType.CRYPTO,
+    baseAsset: "SEI",
+    quoteAsset: "USDT"
+  },
+  {
+    symbol: "KASUSDT",
+    name: "KAS / Tether",
+    type: MarketType.CRYPTO,
+    baseAsset: "KAS",
+    quoteAsset: "USDT"
+  },
+  {
+    symbol: "JASMYUSDT",
+    name: "JASMY / Tether",
+    type: MarketType.CRYPTO,
+    baseAsset: "JASMY",
+    quoteAsset: "USDT"
+  },
+  {
+    symbol: "WIFUSDT",
+    name: "WIF / Tether",
+    type: MarketType.CRYPTO,
+    baseAsset: "WIF",
+    quoteAsset: "USDT"
+  },
+  {
+    symbol: "BONKUSDT",
+    name: "BONK / Tether",
+    type: MarketType.CRYPTO,
+    baseAsset: "BONK",
+    quoteAsset: "USDT"
+  },
+  {
+    symbol: "CRVUSDT",
+    name: "CRV / Tether",
+    type: MarketType.CRYPTO,
+    baseAsset: "CRV",
+    quoteAsset: "USDT"
+  },
+  {
+    symbol: "SNXUSDT",
+    name: "SNX / Tether",
+    type: MarketType.CRYPTO,
+    baseAsset: "SNX",
+    quoteAsset: "USDT"
+  },
+  {
+    symbol: "MANAUSDT",
+    name: "MANA / Tether",
+    type: MarketType.CRYPTO,
+    baseAsset: "MANA",
+    quoteAsset: "USDT"
+  },
+  {
+    symbol: "SANDUSDT",
+    name: "SAND / Tether",
+    type: MarketType.CRYPTO,
+    baseAsset: "SAND",
+    quoteAsset: "USDT"
+  },
+  {
+    symbol: "AXSUSDT",
+    name: "AXS / Tether",
+    type: MarketType.CRYPTO,
+    baseAsset: "AXS",
+    quoteAsset: "USDT"
+  },
+  {
+    symbol: "GALAUSDT",
+    name: "GALA / Tether",
+    type: MarketType.CRYPTO,
+    baseAsset: "GALA",
+    quoteAsset: "USDT"
+  },
+  {
     symbol: "EURUSD",
     name: "Euro / US Dollar",
     type: MarketType.FOREX,
